d10959fe81fc54c6f02b1df73243497d4bcdab89
XiaoLilith <125671807+XiaoLilith@users.noreply.github.com>
2023-03-06 21:23:48 +0700

    Add files via upload

---
+++ Retangle.java
@@ -0,0 +1,17 @@
+package lchapter;
+public class Retangle extends LTChapter {
+    private double width = 1.0;
+    private double length= 1.0;
+    public Retangle(String color, boolean filled) {
+        super(color, filled);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Retangle{");
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}
